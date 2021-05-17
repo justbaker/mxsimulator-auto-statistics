6707b9db723ecbc942aab9e8da4bf7bba0f53a18
William Burkes <dbrider251@gmail.com>
2021-05-17 17:17:31 -0500

    bunch o changes

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
 const puppeteer = require('puppeteer');
-const chalk = require('chalk');
 const fs = require('fs');
+const qualurl = 'https://www.racefactorygaming.com/Events/Event?event=720';
 
-const error = chalk.bold.red;
-const success = chalk.keyword("green");
+const main = async () =>{
+    await qual250();
+    await qual450();
 
+}
 
-(async () => {
-    try{
-        let browser = await puppeteer.launch({headless: true});
-        let page = await browser.newPage();
-        await page.setViewport({width: 1920, height: 1080})
-        await page.goto(`http://mxsimulator.com/servers/official.mxslobby.com:19801/races/8515.html`);
-        await page.waitForTimeout(2000);
-        let results = await page.evaluate(()=>{
-            let position = document.querySelectorAll(`td.pos`);
-            let number = document.querySelectorAll(`td.no`);
-            let name = document.querySelectorAll(`td.name`);
-            let besttime = document.querySelectorAll(`td.besttime`);
-            let resultArray = [];
-            for (let i=0; i<position.length; i++){
-                resultArray[i] = {
-                    position: parseInt(position[i].innerText, 10),
-                    number: parseInt(number[i].innerText, 10),
-                    name: name[i].innerText,
-                    besttime: parseFloat(besttime[i].innerText)
-                };
-            }
-            return {resultArray};
-        })
+main().then(r => {});
 
-       await fs.writeFile("results.json", JSON.stringify(results, undefined, 4), function(err){
-          if (err) throw err;
-          console.log("Saved results");
-       })
+async function qual250(){
+    let browser = await puppeteer.launch({headless: true});
+    let page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080})
+    //const qual250url = 'https://www.racefactorygaming.com/Events/Event?event=720';
+    await page.goto(qualurl);
+    await page.waitForTimeout(2000);
+    fs.writeFile(`${__dirname}/stats.txt`, `[url=${qualurl}][color=#0080BF][b]Top 10 Qualifiers[/b][/color][/url]\n\n[b][u]250 Supercross[/b][/u]\n`, 'utf8', () =>{});
 
-       await browser.close();
-       console.log(success("Browser Closed with success"));
-    } catch (err){
-        console.log(error(err));
-        //await browser.close();
-        console.log(error("Browser Closed"));
+    let qualifying = await page.evaluate(() =>{
+        function capitalize(str) {
+            return str.replace(
+                /\w\S*/g,
+                function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                }
+            );
+        }
+        let numberArray = [];
+        let nameArray = [];
+        let timeArray = [];
+        for(let i=0;i<10;i++){
+            numberArray[i] = document.querySelector(`#DataTables_Table_4 > tbody:nth-child(2) > tr:nth-child(${i+1}) > td:nth-child(2)`).innerHTML;
+            nameArray[i] = capitalize(document.querySelector(`#DataTables_Table_4 > tbody:nth-child(2) > tr:nth-child(${i+1}) > td:nth-child(4)`).innerHTML);
+            timeArray[i] = document.querySelector(`#DataTables_Table_4 > tbody:nth-child(2) > tr:nth-child(${i+1}) > td:nth-child(5)`).innerHTML;
+        }
+        return {numberArray, nameArray, timeArray};
+    });
+    for(let j = 0; j<10;j++){
+        fs.appendFileSync(`${__dirname}/stats.txt`, `${j+1}. [i][size=85]#${qualifying.numberArray[j]}[/size][/i] - ${qualifying.nameArray[j]} | [size=85][color=#FF0000]Team[/color][/size] - [size=85][i]${qualifying.timeArray[j]}[/i][/size]\n`)
     }
-})();
+    await browser.close();
+}
+
+async function qual450(){
+    let browser = await puppeteer.launch({headless: true});
+    let page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080})
+    //const qual450url = 'https://www.racefactorygaming.com/Events/Event?event=726';
+    await page.goto(qualurl);
+    await page.waitForTimeout(2000);
+    fs.appendFileSync(`${__dirname}/stats.txt`, `\n[b][u]450 Supercross[/b][/u]\n`);
+
+    let qualifying = await page.evaluate(() =>{
+        function capitalize(str) {
+            return str.replace(
+                /\w\S*/g,
+                function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                }
+            );
+        }
+        let numberArray = [];
+        let nameArray = [];
+        let timeArray = [];
+        for(let i=0;i<10;i++){
+            numberArray[i] = document.querySelector(`#DataTables_Table_3 > tbody:nth-child(2) > tr:nth-child(${i+1}) > td:nth-child(2)`).innerHTML;
+            nameArray[i] = capitalize(document.querySelector(`#DataTables_Table_3 > tbody:nth-child(2) > tr:nth-child(${i+1}) > td:nth-child(4)`).innerHTML);
+            timeArray[i] = document.querySelector(`#DataTables_Table_3 > tbody:nth-child(2) > tr:nth-child(${i+1}) > td:nth-child(5)`).innerHTML;
+        }
+        return {numberArray, nameArray, timeArray};
+    });
+    for(let j = 0; j<10;j++){
+        fs.appendFileSync(`${__dirname}/stats.txt`, `${j+1}. [i][size=85]#${qualifying.numberArray[j]}[/size][/i] - ${qualifying.nameArray[j]} | [size=85][color=#FF0000]Team[/color][/size] - [size=85][i]${qualifying.timeArray[j]}[/i][/size]\n`)
+    }
+    await browser.close();
+}
